fa759386124b68d3ef3bda820b55f2c6ba78e48f
Dev Shah <devshah580@gmail.com>
2026-05-30 23:12:22 -0500

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<tuple<int, int, int>> heap;
+        for(int i = 0; i < points.size(); i++) {
+            int temp = (points[i][0] * points[i][0]) + (points[i][1] * points[i][1]);
+            heap.push({temp, points[i][0], points[i][1]});
+            if(heap.size() > k) {
+                heap.pop();
+            }
+        }
+
+        vector<vector<int>> result(k);
+        for(int i = 0; i < result.size(); i++) {
+            result[i] = {get<1>(heap.top()), get<2>(heap.top())};
+            heap.pop();
+        }
+        return result;
+    }
+};
