3f85ef8e7e19f657d0554de3760ff31e57babda9
Dev Shah <devshah580@gmail.com>
2026-05-30 22:55:19 -0500

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> heap(stones.begin(), stones.end());
+
+        while(heap.size() > 1) {
+            int y = heap.top();
+            heap.pop();
+            int x = heap.top();
+            heap.pop();
+            if(x != y) {
+                heap.push(y - x);
+            }
+        }
+
+        if(heap.size() == 0) {
+            return 0;
+        } else {
+            return heap.top();
+        }
+    }
+};
