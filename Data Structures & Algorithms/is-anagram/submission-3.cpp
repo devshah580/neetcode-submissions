37697a02b1d1fed348f1c590145e3bcd0ade92ef
Dev Shah <devshah580@gmail.com>
2026-07-08 13:56:01 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) {
+            return false;
+        }
+        std::vector<int> s_freq(26, 0);
+        std::vector<int> t_freq(26, 0);
+        for(int i = 0; i < s.size(); i++) {
+            s_freq[s[i] - 'a']++;
+            t_freq[t[i] - 'a']++;
+        }
+        return s_freq == t_freq;
+    }
+};
